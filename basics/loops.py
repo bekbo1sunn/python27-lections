efce745a79d409bb883990d3037816ac49bf030e
bekbo1sunn <bekbolsun2003asanov@gmail.com>
2023-02-08 12:37:47 +0600

    loops.py

---
+++ basics/loops.py
@@ -0,0 +1,53 @@
+'                                                                           Циклы                                                                                 '
+
+# циклы - блок кода, который отрабатывается несколько раз
+
+# for - цикл, который работает с итерируемыми обьектами, цикл заканчивается,
+# когда он доходит до последнего элемента в итнрируемом обьекте 
+
+# while - цикл, который работает до тех пор пока условие верное 
+
+# list1 = ['hello', 10, True, [1,2,3]]
+# for element in list1: 
+#     print(element)
+    
+# string1 = 'hello world'
+# for letter in string1:
+#     print(letter)
+
+# i = 1
+# while i != 10:
+#     print('hello', i)
+#     i = i + 1 
+    
+# i = 0
+# while i:
+#     print('hello')
+#     i = i + 1
+
+# "                                                                         Ключевые слова в циклах                                                                  "
+# # break - полностью останавливает цикл 
+
+# # continue - переход к следующей итерации 
+
+# for i in range(10):
+#     if i == 3:
+#         break
+#     print(i)
+# # 0, 1, 2
+
+# for i in range(10):
+#     if i == 3:
+#         continue
+#     print(i)
+# # 0, 1, 2, 4, 5, 6, 7, 8, 9
+
+
+list2 = [1, 2, 3, 4, 5, 6, 7,1,1,1,1]
+# list2.clear()
+# print(list2)
+
+while 1 in list2:
+    list2.remove(1)
+print(list2)
+
